d8416b7c3acd55150a15360d6a8626734c9c5207
Shashi Deshetti <deshetti@gmail.com>
2018-10-25 17:11:56 -0500

    Added Entity: Status

---
+++ src/test/java/com/factly/dega/web/rest/StatusResourceIntTest.java
@@ -0,0 +1,344 @@
+package com.factly.dega.web.rest;
+
+import com.factly.dega.CoreApp;
+
+import com.factly.dega.domain.Status;
+import com.factly.dega.repository.StatusRepository;
+import com.factly.dega.repository.search.StatusSearchRepository;
+import com.factly.dega.service.StatusService;
+import com.factly.dega.service.dto.StatusDTO;
+import com.factly.dega.service.mapper.StatusMapper;
+import com.factly.dega.web.rest.errors.ExceptionTranslator;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.Collections;
+import java.util.List;
+
+
+import static com.factly.dega.web.rest.TestUtil.createFormattingConversionService;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.elasticsearch.index.query.QueryBuilders.queryStringQuery;
+import static org.hamcrest.Matchers.hasItem;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+/**
+ * Test class for the StatusResource REST controller.
+ *
+ * @see StatusResource
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = CoreApp.class)
+public class StatusResourceIntTest {
+
+    private static final String DEFAULT_NAME = "AAAAAAAAAA";
+    private static final String UPDATED_NAME = "BBBBBBBBBB";
+
+    private static final String DEFAULT_CLIENT_ID = "AAAAAAAAAA";
+    private static final String UPDATED_CLIENT_ID = "BBBBBBBBBB";
+
+    @Autowired
+    private StatusRepository statusRepository;
+
+    @Autowired
+    private StatusMapper statusMapper;
+    
+    @Autowired
+    private StatusService statusService;
+
+    /**
+     * This repository is mocked in the com.factly.dega.repository.search test package.
+     *
+     * @see com.factly.dega.repository.search.StatusSearchRepositoryMockConfiguration
+     */
+    @Autowired
+    private StatusSearchRepository mockStatusSearchRepository;
+
+    @Autowired
+    private MappingJackson2HttpMessageConverter jacksonMessageConverter;
+
+    @Autowired
+    private PageableHandlerMethodArgumentResolver pageableArgumentResolver;
+
+    @Autowired
+    private ExceptionTranslator exceptionTranslator;
+
+    private MockMvc restStatusMockMvc;
+
+    private Status status;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        final StatusResource statusResource = new StatusResource(statusService);
+        this.restStatusMockMvc = MockMvcBuilders.standaloneSetup(statusResource)
+            .setCustomArgumentResolvers(pageableArgumentResolver)
+            .setControllerAdvice(exceptionTranslator)
+            .setConversionService(createFormattingConversionService())
+            .setMessageConverters(jacksonMessageConverter).build();
+    }
+
+    /**
+     * Create an entity for this test.
+     *
+     * This is a static method, as tests for other entities might also need it,
+     * if they test an entity which requires the current entity.
+     */
+    public static Status createEntity() {
+        Status status = new Status()
+            .name(DEFAULT_NAME)
+            .clientId(DEFAULT_CLIENT_ID);
+        return status;
+    }
+
+    @Before
+    public void initTest() {
+        statusRepository.deleteAll();
+        status = createEntity();
+    }
+
+    @Test
+    public void createStatus() throws Exception {
+        int databaseSizeBeforeCreate = statusRepository.findAll().size();
+
+        // Create the Status
+        StatusDTO statusDTO = statusMapper.toDto(status);
+        restStatusMockMvc.perform(post("/api/statuses")
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .content(TestUtil.convertObjectToJsonBytes(statusDTO)))
+            .andExpect(status().isCreated());
+
+        // Validate the Status in the database
+        List<Status> statusList = statusRepository.findAll();
+        assertThat(statusList).hasSize(databaseSizeBeforeCreate + 1);
+        Status testStatus = statusList.get(statusList.size() - 1);
+        assertThat(testStatus.getName()).isEqualTo(DEFAULT_NAME);
+        assertThat(testStatus.getClientId()).isEqualTo(DEFAULT_CLIENT_ID);
+
+        // Validate the Status in Elasticsearch
+        verify(mockStatusSearchRepository, times(1)).save(testStatus);
+    }
+
+    @Test
+    public void createStatusWithExistingId() throws Exception {
+        int databaseSizeBeforeCreate = statusRepository.findAll().size();
+
+        // Create the Status with an existing ID
+        status.setId("existing_id");
+        StatusDTO statusDTO = statusMapper.toDto(status);
+
+        // An entity with an existing ID cannot be created, so this API call must fail
+        restStatusMockMvc.perform(post("/api/statuses")
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .content(TestUtil.convertObjectToJsonBytes(statusDTO)))
+            .andExpect(status().isBadRequest());
+
+        // Validate the Status in the database
+        List<Status> statusList = statusRepository.findAll();
+        assertThat(statusList).hasSize(databaseSizeBeforeCreate);
+
+        // Validate the Status in Elasticsearch
+        verify(mockStatusSearchRepository, times(0)).save(status);
+    }
+
+    @Test
+    public void checkNameIsRequired() throws Exception {
+        int databaseSizeBeforeTest = statusRepository.findAll().size();
+        // set the field null
+        status.setName(null);
+
+        // Create the Status, which fails.
+        StatusDTO statusDTO = statusMapper.toDto(status);
+
+        restStatusMockMvc.perform(post("/api/statuses")
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .content(TestUtil.convertObjectToJsonBytes(statusDTO)))
+            .andExpect(status().isBadRequest());
+
+        List<Status> statusList = statusRepository.findAll();
+        assertThat(statusList).hasSize(databaseSizeBeforeTest);
+    }
+
+    @Test
+    public void checkClientIdIsRequired() throws Exception {
+        int databaseSizeBeforeTest = statusRepository.findAll().size();
+        // set the field null
+        status.setClientId(null);
+
+        // Create the Status, which fails.
+        StatusDTO statusDTO = statusMapper.toDto(status);
+
+        restStatusMockMvc.perform(post("/api/statuses")
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .content(TestUtil.convertObjectToJsonBytes(statusDTO)))
+            .andExpect(status().isBadRequest());
+
+        List<Status> statusList = statusRepository.findAll();
+        assertThat(statusList).hasSize(databaseSizeBeforeTest);
+    }
+
+    @Test
+    public void getAllStatuses() throws Exception {
+        // Initialize the database
+        statusRepository.save(status);
+
+        // Get all the statusList
+        restStatusMockMvc.perform(get("/api/statuses?sort=id,desc"))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+            .andExpect(jsonPath("$.[*].id").value(hasItem(status.getId())))
+            .andExpect(jsonPath("$.[*].name").value(hasItem(DEFAULT_NAME.toString())))
+            .andExpect(jsonPath("$.[*].clientId").value(hasItem(DEFAULT_CLIENT_ID.toString())));
+    }
+    
+    @Test
+    public void getStatus() throws Exception {
+        // Initialize the database
+        statusRepository.save(status);
+
+        // Get the status
+        restStatusMockMvc.perform(get("/api/statuses/{id}", status.getId()))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+            .andExpect(jsonPath("$.id").value(status.getId()))
+            .andExpect(jsonPath("$.name").value(DEFAULT_NAME.toString()))
+            .andExpect(jsonPath("$.clientId").value(DEFAULT_CLIENT_ID.toString()));
+    }
+
+    @Test
+    public void getNonExistingStatus() throws Exception {
+        // Get the status
+        restStatusMockMvc.perform(get("/api/statuses/{id}", Long.MAX_VALUE))
+            .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void updateStatus() throws Exception {
+        // Initialize the database
+        statusRepository.save(status);
+
+        int databaseSizeBeforeUpdate = statusRepository.findAll().size();
+
+        // Update the status
+        Status updatedStatus = statusRepository.findById(status.getId()).get();
+        updatedStatus
+            .name(UPDATED_NAME)
+            .clientId(UPDATED_CLIENT_ID);
+        StatusDTO statusDTO = statusMapper.toDto(updatedStatus);
+
+        restStatusMockMvc.perform(put("/api/statuses")
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .content(TestUtil.convertObjectToJsonBytes(statusDTO)))
+            .andExpect(status().isOk());
+
+        // Validate the Status in the database
+        List<Status> statusList = statusRepository.findAll();
+        assertThat(statusList).hasSize(databaseSizeBeforeUpdate);
+        Status testStatus = statusList.get(statusList.size() - 1);
+        assertThat(testStatus.getName()).isEqualTo(UPDATED_NAME);
+        assertThat(testStatus.getClientId()).isEqualTo(UPDATED_CLIENT_ID);
+
+        // Validate the Status in Elasticsearch
+        verify(mockStatusSearchRepository, times(1)).save(testStatus);
+    }
+
+    @Test
+    public void updateNonExistingStatus() throws Exception {
+        int databaseSizeBeforeUpdate = statusRepository.findAll().size();
+
+        // Create the Status
+        StatusDTO statusDTO = statusMapper.toDto(status);
+
+        // If the entity doesn't have an ID, it will throw BadRequestAlertException
+        restStatusMockMvc.perform(put("/api/statuses")
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .content(TestUtil.convertObjectToJsonBytes(statusDTO)))
+            .andExpect(status().isBadRequest());
+
+        // Validate the Status in the database
+        List<Status> statusList = statusRepository.findAll();
+        assertThat(statusList).hasSize(databaseSizeBeforeUpdate);
+
+        // Validate the Status in Elasticsearch
+        verify(mockStatusSearchRepository, times(0)).save(status);
+    }
+
+    @Test
+    public void deleteStatus() throws Exception {
+        // Initialize the database
+        statusRepository.save(status);
+
+        int databaseSizeBeforeDelete = statusRepository.findAll().size();
+
+        // Get the status
+        restStatusMockMvc.perform(delete("/api/statuses/{id}", status.getId())
+            .accept(TestUtil.APPLICATION_JSON_UTF8))
+            .andExpect(status().isOk());
+
+        // Validate the database is empty
+        List<Status> statusList = statusRepository.findAll();
+        assertThat(statusList).hasSize(databaseSizeBeforeDelete - 1);
+
+        // Validate the Status in Elasticsearch
+        verify(mockStatusSearchRepository, times(1)).deleteById(status.getId());
+    }
+
+    @Test
+    public void searchStatus() throws Exception {
+        // Initialize the database
+        statusRepository.save(status);
+        when(mockStatusSearchRepository.search(queryStringQuery("id:" + status.getId()), PageRequest.of(0, 20)))
+            .thenReturn(new PageImpl<>(Collections.singletonList(status), PageRequest.of(0, 1), 1));
+        // Search the status
+        restStatusMockMvc.perform(get("/api/_search/statuses?query=id:" + status.getId()))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+            .andExpect(jsonPath("$.[*].id").value(hasItem(status.getId())))
+            .andExpect(jsonPath("$.[*].name").value(hasItem(DEFAULT_NAME.toString())))
+            .andExpect(jsonPath("$.[*].clientId").value(hasItem(DEFAULT_CLIENT_ID.toString())));
+    }
+
+    @Test
+    public void equalsVerifier() throws Exception {
+        TestUtil.equalsVerifier(Status.class);
+        Status status1 = new Status();
+        status1.setId("id1");
+        Status status2 = new Status();
+        status2.setId(status1.getId());
+        assertThat(status1).isEqualTo(status2);
+        status2.setId("id2");
+        assertThat(status1).isNotEqualTo(status2);
+        status1.setId(null);
+        assertThat(status1).isNotEqualTo(status2);
+    }
+
+    @Test
+    public void dtoEqualsVerifier() throws Exception {
+        TestUtil.equalsVerifier(StatusDTO.class);
+        StatusDTO statusDTO1 = new StatusDTO();
+        statusDTO1.setId("id1");
+        StatusDTO statusDTO2 = new StatusDTO();
+        assertThat(statusDTO1).isNotEqualTo(statusDTO2);
+        statusDTO2.setId(statusDTO1.getId());
+        assertThat(statusDTO1).isEqualTo(statusDTO2);
+        statusDTO2.setId("id2");
+        assertThat(statusDTO1).isNotEqualTo(statusDTO2);
+        statusDTO1.setId(null);
+        assertThat(statusDTO1).isNotEqualTo(statusDTO2);
+    }
+}
