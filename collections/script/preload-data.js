890d75f24ccfa4c6426f14b7e9d53a963ecca9a2
Shashi Deshetti <deshetti@gmail.com>
2018-11-12 13:42:46 -0600

    Added sample data

--- collections/script/preload-data.js
+++ collections/script/preload-data.js
@@ -2,11 +2,28 @@
 
 // load org data
 print('Loading organizations data.');
-loadSeedData(db.organizations, JSON.parse(cat('collections/test/organizations.json')));
+loadSeedData(db.organizations, JSON.parse(cat('collections/test/organization.json')));
 
 // load categories data
 print('Loading organizations data.');
-loadSeedData(db.categories, JSON.parse(cat('collections/test/categories.json')));
+loadSeedData(db.categories, JSON.parse(cat('collections/test/category.json')));
+
+// load format data
+print('Loading organizations data.');
+loadSeedData(db.categories, JSON.parse(cat('collections/test/format.json')));
+
+// load roles data
+print('Loading organizations data.');
+loadSeedData(db.categories, JSON.parse(cat('collections/test/role.json')));
+
+// load status data
+print('Loading organizations data.');
+loadSeedData(db.categories, JSON.parse(cat('collections/test/status.json')));
+
+// load tag data
+print('Loading organizations data.');
+loadSeedData(db.categories, JSON.parse(cat('collections/test/tag.json')));
+
 
 // .. so on
 
