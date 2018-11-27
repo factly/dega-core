dff620b83254a3b3c0f12a34788ce214bae6f323
soddiraju <sravan.oddiraju@aa.com>
2018-11-27 10:45:21 -0600

    fixing compilation errors and fixing failing unit tests

--- src/main/java/com/factly/dega/domain/Organization.java
+++ src/main/java/com/factly/dega/domain/Organization.java
@@ -150,7 +150,6 @@ public class Organization implements Serializable {
     private String slug;
 
     @NotNull
-    @Pattern(regexp = "'^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,30}$'")
     @Field("email")
     private String email;
 
--- src/main/java/com/factly/dega/repository/DegaUserRepository.java
+++ src/main/java/com/factly/dega/repository/DegaUserRepository.java
@@ -25,4 +25,7 @@ public interface DegaUserRepository extends MongoRepository<DegaUser, String> {
     @Query("{'id': ?0}")
     Optional<DegaUser> findOneWithEagerRelationships(String id);
 
+    @Query("{'email': ?0}")
+    Optional<DegaUser> findByEmailId(String emailId);
+
 }
